bd04bfc42904d34c5064f5d6d0776b18d979ea26
Jorge de Jesus Tejeda Bello <jtejeda@Jorges-MacBook-Pro.local>
2021-06-20 20:53:40 -0400

    Agregamos un import dinamico en el componente de photocard, ya que la clase IntersectionObserver no es soportada por navegadores viejos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -11,19 +11,22 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   useEffect(() => {
     // console.log(element.current)
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      // console.log(isIntersecting)
-      if (isIntersecting) {
-        console.log(isIntersecting)
-        setShow(true)
-        // Para que el observador no se vuelva actualizar
-        // Cuando aparezcan las que estane n la pantalla
-        observer.disconnect()
-        // OJO, si los elementos que se van a renderizar empiezan teniendo un height 0 para despues adaptarse al tamano mientras esta renderizandose la pagina InterserctionObserver los notara como la propiedad isIntersecting sera true aunque despues que cargue la pagina no vean los demas elementos
-      }
-    })
-    observer.observe(element.current)
+    import('intersection-observer')
+      .then(() => {
+        const observer = new window.IntersectionObserver((entries) => {
+          const { isIntersecting } = entries[0]
+          // console.log(isIntersecting)
+          if (isIntersecting) {
+            console.log(isIntersecting)
+            setShow(true)
+            // Para que el observador no se vuelva actualizar
+            // Cuando aparezcan las que estane n la pantalla
+            observer.disconnect()
+          // OJO, si los elementos que se van a renderizar empiezan teniendo un height 0 para despues adaptarse al tamano mientras esta renderizandose la pagina InterserctionObserver los notara como la propiedad isIntersecting sera true aunque despues que cargue la pagina no vean los demas elementos
+          }
+        })
+        observer.observe(element.current)
+      })
   }, [element])
 
   return (
