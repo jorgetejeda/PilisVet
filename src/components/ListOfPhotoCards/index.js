ef168944ff33403597f5bbd28adf9cead13aa8ca
Jorge de Jesus Tejeda Bello <jtejeda@Jorges-MacBook-Pro.local>
2021-06-21 02:00:56 -0400

    Creamos un hook para leer todas las fotos

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { useGetPhotos } from '../../hooks/useGetPhotos'
 
-import { useQuery, gql } from '@apollo/client'
+export const ListOfPhotoCards = ({ categoryId }) => {
+  const { loading, error, data } = useGetPhotos(categoryId)
 
-const withPhotos = gql`
-  query getPhotos{
-    photos{
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`
-
-export const ListOfPhotoCards = () => {
-  const { loading, error, data } = useQuery(withPhotos)
-
-  if (error) return (<h2>Internal Server Error</h2>)
   if (loading) return (<h2>Loading....</h2>)
+  if (error) return (<h2>Internal Server Error</h2>)
 
   return (
-    <ul style={{ margin: '0 10px' }}>
+    <ul>
       {data.photos.map((photo) => (
         <PhotoCard key={photo.id} {...photo} />
       ))}
